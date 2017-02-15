cd7ea50934b7a1ec5427a956622145f00beb5108
Fanzhong <fanzhong@sunmi.com>
2017-02-15 11:15:41 +0800

    Aidl complete

--- app/src/main/java/demo/fanzhong/com/myservices1/MainActivity.java
+++ app/src/main/java/demo/fanzhong/com/myservices1/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
+import android.os.RemoteException;
 import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,23 +13,42 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import demo.fanzhong.com.myservices1.util.Tool;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button mStart;
     private Button mStop;
     private Button mBtBinder;
     private Button mBtUnbinder;
 
+    private IMyRemoteService mIMyRemoteService;
+
     private MyBinder myBinder;
 
     ServiceConnection conn = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            isBinder = true;
             //myBinder = (MyBinder) iBinder;
             //myBinder.startBinderTest();
+            mIMyRemoteService = IMyRemoteService.Stub.asInterface(iBinder);
+
+            try {
+                int plus = mIMyRemoteService.plus(1, 3);
+                String S = mIMyRemoteService.toUpperCase("hEllo woRLd!");
+                Log.d(Tool.TAG, "plus = " + plus);
+                Log.d(Tool.TAG, "S = " + S);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+            Log.d(Tool.TAG, "isbinder is " + isBinder);
+
         }
 
         @Override
         public void onServiceDisconnected(ComponentName componentName) {
+            isBinder = false;
+            Log.d(Tool.TAG, "isbinder is " + isBinder);
 
         }
     };
@@ -47,6 +67,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mBtUnbinder.setOnClickListener(this);
     }
 
+    private boolean isBinder = false;
     @Override
     public void onClick(View view) {
         switch (view.getId()){
@@ -61,12 +82,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 stopService(stopIntent);
                 break;
             case R.id.start_binder:
-//                Intent startBinder = new Intent(this, MyRemoteService.class);
-                Intent startBinder = new Intent(this, MyService.class);
-                bindService(startBinder, conn, BIND_AUTO_CREATE);
+                if(!isBinder) {
+                    Intent startBinder = new Intent(this, MyRemoteService.class);
+                    //                Intent startBinder = new Intent(this, MyService.class);
+                    bindService(startBinder, conn, BIND_AUTO_CREATE);
+                }
                 break;
             case R.id.stop_binder:
-                unbindService(conn);
+                if(isBinder){
+                    unbindService(conn);
+                    isBinder = false;
+                }
                 break;
             default:
                 break;
--- app/src/main/java/demo/fanzhong/com/myservices1/MyRemoteService.java
+++ app/src/main/java/demo/fanzhong/com/myservices1/MyRemoteService.java
@@ -4,22 +4,37 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.os.Process;
+import android.os.RemoteException;
 import android.util.Log;
 
 import demo.fanzhong.com.myservices1.util.Tool;
 
 public class MyRemoteService extends Service {
+    IMyRemoteService.Stub mBinder = new IMyRemoteService.Stub() {
+        @Override
+        public int plus(int a, int b) throws RemoteException {
+            return a+b;
+        }
+
+        @Override
+        public String toUpperCase(String str) throws RemoteException {
+            if(null != str){
+                str = str.toUpperCase();
+            }
+            return str;
+        }
+    };
 
     @Override
     public void onCreate() {
         super.onCreate();
         Log.d(Tool.TAG, "MyRemoteService onCreate() executed");
-        try {
-            Thread.sleep(6000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Log.d(Tool.TAG, "process id is " + Process.myPid());
+//        try {
+//            Thread.sleep(6000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        Log.d(Tool.TAG, "process id is " + Process.myPid());
     }
 
     @Override
@@ -31,10 +46,12 @@ public class MyRemoteService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d(Tool.TAG, "MyRemoteService onDestroy() executed");
+
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return mBinder;
     }
 }
